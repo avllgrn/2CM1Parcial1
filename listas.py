c9868c55d73f704dbf314be604139cadf5d123d8
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-14 10:19:03 -0600

    Multiplicacion acumulada

--- listas.py
+++ listas.py
@@ -5,9 +5,9 @@ if __name__ == '__main__':
     
     n = int(input('Dame n '))
     
-    s = 0
-    for i in range(n+1):
-        s = s+i
-        print(f'i = {i}\ts = {s}')
+    p = 1
+    for i in range(1, n+1):
+        p = p*i
+        print(f'i = {i}\tp = {p}')
 
-    print(f'\n\ns = {s}')
+    print(f'\n\np = {p}')
