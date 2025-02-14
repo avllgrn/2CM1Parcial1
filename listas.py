d8f3d8c8ab1b8290940b8fc7d5f5b480de4c4ea6
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-14 10:15:30 -0600

    Suma acumulada

--- listas.py
+++ listas.py
@@ -1,32 +1,13 @@
 from os import system
-from random import randrange
-
-def unaFuncionConInput():
-    V = []
-    print(V, type(V), len(V))
-
-    n = int(input('Cuantos? '))
-
-    for i in range(n):
-        dato = float(input(f'Ingresa el dato {i} '))
-        V.append(dato)
-        print(i, V[i], type(V[i]))
-    print(V, type(V), len(V))
-
-def unaFuncionConRandRange():
-    V = []
-    print(V, type(V), len(V))
-
-    n = int(input('Cuantos? '))
-
-    for i in range(n):
-        V.append(randrange(101)/10)
-        print(i, V[i], type(V[i]))
-    print(V, type(V), len(V))
 
 if __name__ == '__main__':
     system('cls')
-    unaFuncionConInput()
-    print()
-    unaFuncionConRandRange()
-    print()
+    
+    n = int(input('Dame n '))
+    
+    s = 0
+    for i in range(n+1):
+        s = s+i
+        print(f'i = {i}\ts = {s}')
+
+    print(f'\n\ns = {s}')
