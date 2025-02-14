f7649321e5f9ed570a5bf96624ad200b8f0b9b06
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-14 10:24:07 -0600

    Suma y multiplicacion de aleatorios

--- listas.py
+++ listas.py
@@ -1,13 +1,23 @@
 from os import system
+from random import randrange
 
 if __name__ == '__main__':
     system('cls')
     
     n = int(input('Dame n '))
     
+    s = 0
+    for i in range(n+1):
+        dato = randrange(1, 10)
+        s = s+dato
+        print(f'dato = {dato}\ts = {s}')
+
+    print(f'\ns = {s}\n\n')
+
     p = 1
     for i in range(1, n+1):
-        p = p*i
-        print(f'i = {i}\tp = {p}')
+        dato = randrange(1, 10)
+        p = p*dato
+        print(f'dato = {dato}\tp = {p}')
 
-    print(f'\n\np = {p}')
+    print(f'\np = {p}')
