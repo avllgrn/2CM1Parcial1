cec3f3731c292252cdbdcf980069342ffcf57b70
avllgrn <avllgrn@ipn.mx>
2025-02-25 10:45:45 -0600

    Inicializar un conjunto y agregar elementos

--- listas.py
+++ listas.py
@@ -1,20 +1,24 @@
 from os import system
-from random import randrange
-
-def funcion(*argumentos):
-    print(f'argumentos = {argumentos} tamaño = {len(argumentos)} type = {type(argumentos)}')
-    print('Contenido de argumentos:')
-    for dato in argumentos:
-        print(f'{dato} type = {type(dato)}')
-    print()
 
 if __name__ == '__main__':
     system('cls')
 
-    funcion()
-    funcion(1)
-    funcion(2.3)
-    funcion('cuatro')
-    funcion(5, 6, 7, 8)
-    funcion(9.8, 7.6, 5.4, 3.2, 1.0)
-    funcion('uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis' )
+    conjunto = set()    #Conjunto vacio ***Cuidado*** NO es {}
+    print(f'conjunto={conjunto} type={type(conjunto)}', end=' ')
+    print(f'len={len(conjunto)}',end='\n\n')
+
+    conjunto = {1, 3, 5, 7, 9}  # Conjunto inicializado
+    print(f'conjunto={conjunto} type={type(conjunto)}', end=' ')
+    print(f'len={len(conjunto)}',end='\n\n')
+
+    conjunto.add(11)    #Se agrega un elemento a un conjunto
+    print(f'conjunto={conjunto} type={type(conjunto)}', end=' ')
+    print(f'len={len(conjunto)}',end='\n\n')
+
+    conjunto.add(3) #No pueden agregarse elementos ya existentes
+    print(f'conjunto={conjunto} type={type(conjunto)}', end=' ')
+    print(f'len={len(conjunto)}',end='\n\n')
+   
+    conjunto.add(7)
+    print(f'conjunto={conjunto} type={type(conjunto)}', end=' ')
+    print(f'len={len(conjunto)}',end='\n\n')    
