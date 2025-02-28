8fa68640370398eec96629a4c4ef540570a7fac9
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-02-28 08:15:17 -0600

    Menu de conjuntos

--- listas.py
+++ listas.py
@@ -1,49 +1,88 @@
 from os import system
 from random import randrange
 
+def creaPares(n):
+
+    pares = set()
+    while len(pares) < n:
+        x = randrange(101)
+        # print(f'Intenta ingresarse {x} ')
+        if x%2 == 0:
+            pares.add(x)
+        #print(pares, type(pares), len(pares))    
+    #print(pares, type(pares), len(pares))
+
+    for numero in pares:
+        print(numero)
+
+def creaImpares(n):
+
+    impares = set()
+    while len(impares) < n:
+        x = randrange(101)
+        #print(f'Intenta ingresarse {x} ')
+        if x%2 != 0:
+            impares.add(x)
+        #print(impares, type(impares), len(impares))
+
+    #print(impares, type(impares), len(impares))
+
+    for numero in impares:
+        print(numero)
+
+def esPrimo(x):
+
+    if x < 2:
+        return False
+
+    for i in range(2, x):
+        if x%i == 0:
+            return False        
+    return True
+
+def creaPrimos(n):
+    primos = set()
+
+    while len(primos) < n:
+        x = randrange(101)
+        # print(f'Intenta ingresarse {x} ')
+        if esPrimo(x):
+            primos.add(x)
+        # print(primos, type(primos), len(primos))
+    # print(primos, type(primos), len(primos))
+
+    for numero in primos:
+        print(numero)
+
+def menu():
+    print('1. Pares')
+    print('2. Impares')
+    print('3. Primos')
+    print('4. Salir')
+    return input('Cuál es tu opción? ')    
+
+def casos(opcion):
+    match opcion:
+        case '1':
+            creaPares(int(input('Cuántos? ')))
+            system('pause')
+        case '2':
+            creaImpares(int(input('Cuántos? ')))
+            system('pause')
+        case '3':
+            creaPrimos(int(input('Cuántos? ')))
+            system('pause')
+        case '4':
+            print('Adiós!')
+        case _:
+            print('Opción inválida!')
+            system('pause')
+
 if __name__ == '__main__':
     system('cls')
-
-    A = set()    #Conjunto vacio ***Cuidado*** NO es {}
-    print(f'conjunto={A} type={type(A)}', end=' ')
-    print(f'len={len(A)}',end='\n\n')
-
-    while len(A)<10:
-        x = randrange(101)
-        print(f'Se intenta agregar {x} en A')
-        A.add(x)
-        print(f'conjunto={A} type={type(A)}', end=' ')
-        print(f'len={len(A)}',end='\n\n')
-        
-    B = set()    #Conjunto vacio ***Cuidado*** NO es {}
-    print(f'conjunto={B} type={type(B)}', end=' ')
-    print(f'len={len(B)}',end='\n\n')
-
-    while len(B)<10:
-        x = randrange(101)
-        print(f'Se intenta agregar {x} en B')
-        B.add(x)
-        print(f'conjunto={B} type={type(B)}', end=' ')
-        print(f'len={len(B)}',end='\n\n')
-
-    print(f'A{A}')
-    for dato in A:
-        print(dato)
-    print()
-
-    print(f'B{B}')
-    for dato in B:
-        print(dato)
-    print()
-
-    U = A.union(B)
-    print(f'A U B={U}')
-    for dato in U:
-        print(dato)
-    print()
-
-    I = A.intersection(B)
-    print(f'A ∩ B={I}')
-    for dato in I:
-        print(dato)
-    print()
+    
+    opcion = None
+    while opcion!='4':
+        system('cls')
+        opcion = menu()
+        casos(opcion)
