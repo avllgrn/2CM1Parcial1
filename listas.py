00775b0a7a003f1d65c1fa00b8b2e16f82df1d79
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-03-01 05:57:23 -0600

    remove, discard y pop para Conjuntos

--- listas.py
+++ listas.py
@@ -1,88 +1,26 @@
 from os import system
 from random import randrange
 
-def creaPares(n):
-
-    pares = set()
-    while len(pares) < n:
-        x = randrange(101)
-        # print(f'Intenta ingresarse {x} ')
-        if x%2 == 0:
-            pares.add(x)
-        #print(pares, type(pares), len(pares))    
-    #print(pares, type(pares), len(pares))
-
-    for numero in pares:
-        print(numero)
-
-def creaImpares(n):
-
-    impares = set()
-    while len(impares) < n:
-        x = randrange(101)
-        #print(f'Intenta ingresarse {x} ')
-        if x%2 != 0:
-            impares.add(x)
-        #print(impares, type(impares), len(impares))
-
-    #print(impares, type(impares), len(impares))
-
-    for numero in impares:
-        print(numero)
-
-def esPrimo(x):
-
-    if x < 2:
-        return False
-
-    for i in range(2, x):
-        if x%i == 0:
-            return False        
-    return True
-
-def creaPrimos(n):
-    primos = set()
-
-    while len(primos) < n:
-        x = randrange(101)
-        # print(f'Intenta ingresarse {x} ')
-        if esPrimo(x):
-            primos.add(x)
-        # print(primos, type(primos), len(primos))
-    # print(primos, type(primos), len(primos))
-
-    for numero in primos:
-        print(numero)
-
-def menu():
-    print('1. Pares')
-    print('2. Impares')
-    print('3. Primos')
-    print('4. Salir')
-    return input('Cuál es tu opción? ')    
-
-def casos(opcion):
-    match opcion:
-        case '1':
-            creaPares(int(input('Cuántos? ')))
-            system('pause')
-        case '2':
-            creaImpares(int(input('Cuántos? ')))
-            system('pause')
-        case '3':
-            creaPrimos(int(input('Cuántos? ')))
-            system('pause')
-        case '4':
-            print('Adiós!')
-        case _:
-            print('Opción inválida!')
-            system('pause')
-
 if __name__ == '__main__':
     system('cls')
+
+    Conjunto = set()
+
+    while len(Conjunto)<10:
+        Conjunto.add(randrange(101))
     
-    opcion = None
-    while opcion!='4':
-        system('cls')
-        opcion = menu()
-        casos(opcion)
+    print(Conjunto, type(Conjunto), len(Conjunto))
+
+    x = int(input('Cuál quieres eliminar? '))
+    if x in Conjunto:
+        Conjunto.remove(x)
+    print(Conjunto, type(Conjunto), len(Conjunto),'\n')
+
+    x = int(input('Cuál quieres eliminar? '))
+    Conjunto.discard(x)
+    print(Conjunto, type(Conjunto), len(Conjunto),'\n')
+
+    x = Conjunto.pop()
+    print(f'Sale {x}', end=' ')
+    Conjunto.discard(x)
+    print(Conjunto, type(Conjunto), len(Conjunto),'\n')
