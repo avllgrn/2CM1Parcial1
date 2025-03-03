eb4fe35950e0cb70aaa86b31f566dbd9b21abdf6
avllgrn <avllgrn@ipn.mx>
2025-03-03 09:45:56 -0600

    Diccionario de dias

--- listas.py
+++ listas.py
@@ -3,34 +3,23 @@ from os import system
 if __name__ == '__main__':
     system('cls')
 
-    d = {}
-    print(d, type(d), len(d), '\n') # muestra el diccionario
+    dias = {1:'Lunes', 2:'Martes', 3:'Miércoles', 4:'Jueves', 5:'Viernes', 6:'Sábado', 7:'Domingo'}
+    print(dias, type(dias), len(dias), '\n') # muestra el diccionario
     
-    d.update({'clave1':'valor1'})
-    print(d, type(d), len(d), '\n') # muestra el diccionario
-    
-    d.update({'clave2':'valor2'})
-    print(d, type(d), len(d), '\n') # muestra el diccionario
-
-    d.update({'clave3':'valor3'})
-    print(d, type(d), len(d), '\n') # muestra el diccionario
-    
-    print(d.items())    # muestra los elementos del diccionario (lista de tuplas)
-    for i in d.items(): 
+    print(dias.items())    # muestra los elementos del diccionario (lista de tuplas)
+    for i in dias.items(): 
         print(i, type(i))    # muestra cada item/tupla/pareja
     print()
 
-    for k,v in d.items(): 
+    for k,v in dias.items(): 
         print(k, type(k), ', ', v, type(v))    # muestra cada clave y valor (despempaquetados) de cada item/tupla/pareja
     print()
 
-    print(d.keys()) # muestra cada clave de cada item/tupla/pareja
-    for k in d.keys():
+    print(dias.keys()) # muestra cada clave de cada item/tupla/pareja
+    for k in dias.keys():
         print(k, type(k))
     print()
 
-    print(d.values())   # muestra cada valor de cada item/tupla/pareja
-    for v in d.values():
+    print(dias.values())   # muestra cada valor de cada item/tupla/pareja
+    for v in dias.values():
         print(v, type(v))
-    
-    
