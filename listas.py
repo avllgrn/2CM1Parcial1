49ffea5df760a7413c1375e0445d8a8f3e537577
avllgrn <avllgrn@ipn.mx>
2025-03-03 09:47:38 -0600

    get() para diccionarios de dias y meses

--- listas.py
+++ listas.py
@@ -1,25 +1,16 @@
 from os import system
+from random import randrange
 
 if __name__ == '__main__':
     system('cls')
 
+    dias = {1:'Lunes', 2:'Martes', 3:'Miércoles', 4:'Jueves', 5:'Viernes', 6:'Sábado', 7:'Domingo'}
     meses = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
-    print(meses, type(meses), len(meses), '\n') # muestra el diccionario
-    
-    print(meses.items())    # muestra los elementos del diccionario (lista de tuplas)
-    for i in meses.items(): 
-        print(i, type(i))    # muestra cada item/tupla/pareja
+
+    for i in range(1, 8):
+        print(dias.get(i))
     print()
 
-    for k,v in meses.items(): 
-        print(k, type(k), ', ', v, type(v))    # muestra cada clave y valor (despempaquetados) de cada item/tupla/pareja
+    for i in range(1, 13):
+        print(meses.get(i))
     print()
-
-    print(meses.keys()) # muestra cada clave de cada item/tupla/pareja
-    for k in meses.keys():
-        print(k, type(k))
-    print()
-
-    print(meses.values())   # muestra cada valor de cada item/tupla/pareja
-    for v in meses.values():
-        print(v, type(v))
