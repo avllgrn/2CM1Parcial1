44d330e5ff2f55dd3698609d0688686a24d85b6e
avllgrn <avllgrn@ipn.mx>
2025-03-03 12:31:52 -0600

    Archivo con "diccionarios"

--- listas.py
+++ listas.py
@@ -8,5 +8,20 @@ if __name__ == '__main__':
     meses = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
 
     n = int(input('Cuántos valores? '))
-    for i in range(n):
-        print(dias.get(randrange(1,8)), 'de', meses.get(randrange(1,13)))
+
+    with open('datos.txt','w') as archivo:
+        for i in range(n):
+            d = randrange(1,8)
+            m = randrange(1,13)
+            archivo.writelines(str(d)+', '+str(m) + '\n')
+            print(dias.get(d), 'de', meses.get(m))
+    print('\n')
+
+    with open('datos.txt','r') as archivo:
+        for i in range(n):
+            linea = archivo.readline().strip().split(',')
+            #print(linea)
+            d = int(linea[0])
+            m = int(linea[1])
+            print(dias.get(d), 'de', meses.get(m))
+    print('\n')
