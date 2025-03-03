b4d72d663c4e185e83ecf75dae6515193a8e0b2b
avllgrn <avllgrn@ipn.mx>
2025-03-03 09:48:33 -0600

    dias y meses aleatorios

--- listas.py
+++ listas.py
@@ -7,10 +7,6 @@ if __name__ == '__main__':
     dias = {1:'Lunes', 2:'Martes', 3:'Miércoles', 4:'Jueves', 5:'Viernes', 6:'Sábado', 7:'Domingo'}
     meses = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
 
-    for d in range(1, 8):
-        print(dias.get(d), dias[d])
-    print()
-
-    for m in range(1, 13):
-        print(meses.get(m), meses[m])
-    print()
+    n = int(input('Cuántos valores? '))
+    for i in range(n):
+        print(dias.get(randrange(1,8)), 'de', meses.get(randrange(1,13)))
