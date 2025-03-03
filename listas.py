7aa7d2fe75eee3fa2974af1f4ab294af33615575
avllgrn <avllgrn@ipn.mx>
2025-03-03 09:46:30 -0600

    Diccionario de meses

--- listas.py
+++ listas.py
@@ -3,23 +3,23 @@ from os import system
 if __name__ == '__main__':
     system('cls')
 
-    dias = {1:'Lunes', 2:'Martes', 3:'Miércoles', 4:'Jueves', 5:'Viernes', 6:'Sábado', 7:'Domingo'}
-    print(dias, type(dias), len(dias), '\n') # muestra el diccionario
+    meses = {1:'Enero', 2:'Febrero', 3:'Marzo', 4:'Abril', 5:'Mayo', 6:'Junio', 7:'Julio', 8:'Agosto', 9:'Septiembre', 10:'Octubre', 11:'Noviembre', 12:'Diciembre'}
+    print(meses, type(meses), len(meses), '\n') # muestra el diccionario
     
-    print(dias.items())    # muestra los elementos del diccionario (lista de tuplas)
-    for i in dias.items(): 
+    print(meses.items())    # muestra los elementos del diccionario (lista de tuplas)
+    for i in meses.items(): 
         print(i, type(i))    # muestra cada item/tupla/pareja
     print()
 
-    for k,v in dias.items(): 
+    for k,v in meses.items(): 
         print(k, type(k), ', ', v, type(v))    # muestra cada clave y valor (despempaquetados) de cada item/tupla/pareja
     print()
 
-    print(dias.keys()) # muestra cada clave de cada item/tupla/pareja
-    for k in dias.keys():
+    print(meses.keys()) # muestra cada clave de cada item/tupla/pareja
+    for k in meses.keys():
         print(k, type(k))
     print()
 
-    print(dias.values())   # muestra cada valor de cada item/tupla/pareja
-    for v in dias.values():
+    print(meses.values())   # muestra cada valor de cada item/tupla/pareja
+    for v in meses.values():
         print(v, type(v))
