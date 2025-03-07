5a8f4fb4c58af1a0c88d26c3cf25d6d59a3e6272
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-03-07 10:55:46 -0600

    Clase Nodo y Clase Cola

--- listas.py
+++ listas.py
@@ -1,47 +1,62 @@
 from os import system
 
 class Nodo:
-    def __init__(self, dato=None, inferior=None):
+    def __init__(self, dato=None, siguiente=None):
         self.dato = dato
-        self.inferior =inferior
+        self.siguiente =siguiente
 
     def __str__(self):
         cadena = '| '
         cadena += str(self.dato) + ' |'
-        if self.inferior != None:
+        if self.siguiente != None:
             cadena += ' -> '
         return cadena
 
-class Pila:
+class Cola:
     def __init__(self):
-        self.tope = None
+        self.primero = None
+        self.ultimo = None
 
     def __del__(self):
         self.liberaMemoria()
-
-    def push(self, dato):
-        self.tope = Nodo(dato, self.tope)
-
-    def pop(self):
-        d = None
-        if not self.estaVacia():
-            d = self.tope.dato
-            aux = self.tope
-            self.tope = self.tope.inferior
-            del aux
-        return d
 
     def liberaMemoria(self):
         while not self.estaVacia():
             print(self.pop())
 
     def estaVacia(self):
-        return self.tope == None
+        return self.primero==None and self.ultimo==None
 
+    def push(self, dato):
+        if self.estaVacia():
+            self.primero = Nodo(dato, None)
+            self.ultimo = self.primero
+
+        else:
+            self.ultimo.siguiente = Nodo(dato, None)
+            self.ultimo = self.ultimo.siguiente
+
+    def pop(self):
+        dato = None
+        if not self.estaVacia():
+            dato = self.primero.dato
+            if self.primero != self.ultimo:
+                aux = self.primero
+    
+                self.primero = self.primero.siguiente
+                del aux
+
+            else:
+                del self.primero
+                self.primero = None
+                self.ultimo = None
+
+        return dato
+    
 if __name__ == '__main__':
     system('cls')
     
-    P = Pila()
+    P = Cola()
 
     P.push(5)
     P.push(3)
@@ -52,6 +67,7 @@ if __name__ == '__main__':
     print(P.pop())
     
     P.push(9)
+
     print(P.pop())
     print(P.pop())
     print(P.pop())
