3a91caaa081627f43afae65909afea1917359eef
avllgrn <avillagran@ipn.mx>
2025-03-10 11:30:36 -0600

    Capicua (suponiendo que existen Pïlas.py y Colas.py con las clases)

--- listas.py
+++ listas.py
@@ -1,15 +1,27 @@
-import Pilas
 from os import system
+import Pilas
+import Colas
+
+def esCapicua(numero):
+    P = Pilas.Pila()
+    C = Colas.Cola()
+
+    for digito in numero:
+        P.push(digito)
+        C.push(digito)
+
+    while not C.estaVacia() and not P.estaVacia():
+        if P.pop() != C.pop():
+            return False
+    return True
 
 if __name__ == '__main__':
     system('cls')
-    P = Pilas.Pila()
-    decimal = int(input('Ingresa numero base diez '))
     
-    x = decimal
-    while x>0:
-        P.push(x%2)
-        x //= 2
-        
-    while not P.estaVacia():
-        print(P.pop(), end='')
+    numero = input('Ingresa numero ')
+    
+    if esCapicua(numero):
+        print(f'{numero} ES capicúa =)')
+    else:
+        system('cls')
+        print(f'{numero} NO es capicúa =(')
