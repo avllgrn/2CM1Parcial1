787bec99773cc2ac5428d61286acfe1ab253658c
Alfredo Villagran Olguin <avillagran@ipn.mx>
2025-03-17 15:40:11 -0600

    Invertir una pila

--- listas.py
+++ listas.py
@@ -1,33 +1,27 @@
 from os import system
+from random import randrange
 import Pilas
-
-def verificaAgrupador(expresion):
-    P = Pilas.Pila()
-
-    parejas = { '(':')', '[':']', '{':'}', '<':'>' }
-
-    for agrupador in expresion:
-        if agrupador in parejas:
-            P.push( parejas.get(agrupador) )
-
-        elif agrupador in parejas.values() and P.estaVacia():
-            return False
-        
-        elif agrupador in parejas.values() and not P.estaVacia() and P.pop()!=agrupador:
-            return False
-
-    if P.estaVacia():
-        return True
-    else:
-        return False
 
 if __name__ == '__main__':
     system('cls')
-    
-    expresion = input('Ingresa expresión ')
-    
-    if verificaAgrupador(expresion):
-        print(f'{expresion} ESTÁ balanceada =)')
-    else:
-        system('cls')
-        print(f'{expresion} NO está balanceada =(')
+
+    n = int(input('Cuántos? '))
+
+    Original = Pilas.Pila()
+
+    for i in range(n):
+        x = randrange(101)
+        print(f'Original.push({x})')
+        Original.push(x)
+    print()
+
+
+    Invertida = Pilas.Pila()
+
+    while not Original.estaVacia():
+        Invertida.push( Original.pop() )
+
+    print('\nOriginal')
+    Original.liberaMemoria()
+    print('\nInvertida')
+    Invertida.liberaMemoria()
