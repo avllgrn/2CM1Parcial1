d56d4370177c08199bf07427c3aa0af627007f13
avllgrn <avillagran@ipn.mx>
2025-03-18 11:23:39 -0600

    Pasar dos conjuntos como claves y valores de un diccionario

--- listas.py
+++ listas.py
@@ -12,6 +12,7 @@ if __name__ == '__main__':
     conjunto2 = set()
     lista1 = []
     lista2 = []
+    diccionario = {}
         
     for i in range(n):
         x = randrange(100)
@@ -22,13 +23,15 @@ if __name__ == '__main__':
 
     print(f'conjunto1 = {conjunto1}')
     print(f'conjunto2 = {conjunto2}')
-    
-    for i in conjunto1:
-        lista1.append(i)
 
-    for i in conjunto2:
-        lista2.append(i)
+    if len(conjunto1) == len(conjunto2):
+        for i in conjunto1:
+            lista1.append(i)
 
-    print(f'lista1 = {lista1}')
-    print(f'lista2 = {lista2}')
-    
+        for i in conjunto2:
+            lista2.append(i)
+            
+        for i in range(len(lista1)):
+            diccionario.update( { lista1[i] : lista2[i]} )
+
+    print(f'diccionario = {diccionario}')
