e08dbc034110d1c4fe23125e1d0bab34f74c902f
avllgrn <avillagran@ipn.mx>
2025-03-18 11:18:42 -0600

    Pasar dos listas como claves y valores de un diccionario

--- listas.py
+++ listas.py
@@ -1,31 +1,28 @@
 from os import system
 from random import randrange
 import Pilas
+import Colas
 
 if __name__ == '__main__':
     system('cls')
 
     n = int(input('Cuántos? '))
 
-    Original = Pilas.Pila()
-    Copia = Pilas.Pila()
-    Auxiliar = Pilas.Pila()
-
+    lista1 = []
+    lista2 = []
+    diccionario = {}
+    
     for i in range(n):
-        x = randrange(101)
-        print(f'Original.push({x})')
-        Original.push(x)
-    print()
-
-    while not Original.estaVacia():
-        Auxiliar.push( Original.pop() )
-
-    while not Auxiliar.estaVacia():
-        temp =Auxiliar.pop()
-        Original.push( temp )
-        Copia.push( temp )
-
-    print('\nOriginal')
-    Original.liberaMemoria()
-    print('\nCopia')
-    Copia.liberaMemoria()
+        x = randrange(100)
+        y = randrange(100)
+        print(f'Se agregan {x} y {y}')
+        lista1.append(x)
+        lista2.append(y)
+        
+    print(f'Lista1 = {lista1}')
+    print(f'Lista2 = {lista2}')
+    
+    for i in range(len(lista1)):
+        diccionario.update({lista1[i] : lista2[i]})
+        
+    print(f'diccionario = {diccionario}')
